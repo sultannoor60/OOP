2a6ceaebc15218dc88b80a16b676a0582db2af13
Sultan Noor <57532679+sultannoor60@users.noreply.github.com>
2019-11-17 13:34:21 +0500

    Add files via upload

---
+++ Chapter 2/2.18.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main(){
+	int a,b;
+	cout<<"enter two numbers: ";
+	cin>>a;
+	cin>>b;
+	if(a!=b){
+		cout<<"These numbers are not equal"<<endl;
+	}
+	if(a<b){
+		cout<<a<<" is smaller";
+	}else{
+			cout<<b<<" is smaller";
+		}
+    
+ 
+	
+	
+	return 0;
+}
