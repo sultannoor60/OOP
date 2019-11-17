677e0b7a9f11660827c6d91bd0c220b0cbe2340c
Sultan Noor <57532679+sultannoor60@users.noreply.github.com>
2019-11-17 14:28:30 +0500

    Update 2.26.cpp

--- Chapter 2/2.26.cpp
+++ Chapter 2/2.26.cpp
@@ -1,3 +1,9 @@
+
+2.26 (Checkerboard Pattern) Display the following checkerboard pattern with eight output
+statements, then display the same pattern using as few statements as possible.
+
+
+
 #include<iostream>
 using namespace std;
 main(){
