8b04e6213efbd10f7efa6358a28d6c5c947d6ffd
Sultan Noor <57532679+sultannoor60@users.noreply.github.com>
2019-11-17 13:34:54 +0500

    Add files via upload

---
+++ Chapter 2/2.16.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main(){
+	int a,b,sum,pro,dif;
+	float quo;
+	cout<<"enter two numbers: ";
+	cin>>a;cin>>b;
+	sum=a+b;
+	cout<<"sum: "<<sum<<endl;
+	pro=a*b;
+	cout<<"product: "<<pro<<endl;
+	dif=a-b;
+	cout<<"difference: "<<dif<<endl;
+	quo=(a/b);
+	cout<<"quotient: "<<quo<<endl;
+    
+ 
+	
+	
+	return 0;
+}
