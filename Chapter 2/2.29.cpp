bc0254c67075e2b9ef9bf47a0e7e92ae63e3b3df
Sultan Noor <57532679+sultannoor60@users.noreply.github.com>
2019-11-17 14:32:15 +0500

    Update 2.29.cpp

--- Chapter 2/2.29.cpp
+++ Chapter 2/2.29.cpp
@@ -1,3 +1,9 @@
+
+2.29 (Table) Using the techniques of this chapter, write a program that calculates the squares and
+cubes of the integers from 0 to 10. Use tabs to print the following neatly formatted table of values:
+
+
+
 #include<iostream>
 using namespace std;
 main(){
