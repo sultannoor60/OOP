e741036f52485e4c30649b05aa121aa970fc4500
Sultan Noor <57532679+sultannoor60@users.noreply.github.com>
2019-11-17 13:39:36 +0500

    Add files via upload

---
+++ Chapter 2/2.21.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+	cout<<"  CCC   +      +   "<<endl;
+	cout<<" C      +      +   "<<endl;
+	cout<<"C     +++++  +++++ "<<endl;
+	cout<<" C      +      +   "<<endl;
+	cout<<"  CCC   +      +   "<<endl;
+	
+	return 0;
+}
