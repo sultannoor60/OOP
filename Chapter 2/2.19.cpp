1f155b8a0e266f4419f1c045472dc1fa2a86acca
Sultan Noor <57532679+sultannoor60@users.noreply.github.com>
2019-11-17 14:23:56 +0500

    Update 2.19.cpp

--- Chapter 2/2.19.cpp
+++ Chapter 2/2.19.cpp
@@ -1,3 +1,16 @@
+
+2.19 (Arithmetic, Smallest and Largest) Write a program that inputs three integers from the keyboard and prints the sum, average, product, smallest and largest of these numbers. The screen dialog
+should appear as follows:
+
+Input three different integers: 13 27 14
+Sum is 54
+Average is 18
+Product is 4914
+Smallest is 13
+Largest is 27
+
+
+
 #include<iostream>
 using namespace std;
 main(){
