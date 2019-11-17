0078263afaba4e9c7e377601f7c9f4e7b129406f
Sultan Noor <57532679+sultannoor60@users.noreply.github.com>
2019-11-17 13:38:04 +0500

    Update 2.18.cpp

--- Chapter 2/2.18.cpp
+++ Chapter 2/2.18.cpp
@@ -1,3 +1,9 @@
+2.18 (Comparing Integers) Write a program that asks the user to enter two integers, obtains the
+numbers from the user, then prints the larger number followed by the words "is larger." If the
+numbers are equal, print the message "These numbers are equal."
+
+
+
 #include<iostream>
 using namespace std;
 main(){
