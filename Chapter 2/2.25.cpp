f0f53d801503f515a2533efa9538f265e5848885
Sultan Noor <57532679+sultannoor60@users.noreply.github.com>
2019-11-17 14:27:53 +0500

    Update 2.25.cpp

--- Chapter 2/2.25.cpp
+++ Chapter 2/2.25.cpp
@@ -1,3 +1,7 @@
+2.25 (Multiples) Write a program that reads in two integers and determines and prints if the first
+is a multiple of the second.
+
+
 #include<iostream>
 using namespace std;
 main(){
