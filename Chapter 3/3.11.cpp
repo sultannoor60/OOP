fd04abf22afa72127bb632387db3aec802572f53
Sultan Noor <57532679+sultannoor60@users.noreply.github.com>
2019-11-28 21:57:29 +0500

    Update 3.11.cpp

--- Chapter 3/3.11.cpp
+++ Chapter 3/3.11.cpp
@@ -1,71 +1,35 @@
-#include <iostream>
-#include <string>
-
-class MotorVehicle {
-
-public:
-
-   MotorVehicle( std::string m, std::string ft, int y ) {
-      setMake( m );
-      setFuelType( ft );
-      setYearOfManufacture( y );
-   }
-
-   void setMake( std::string m ) {
-      make = m;
-   }
-
-   void setFuelType( std::string ft ) {
-      fuelType = ft;
-   }
-
-   void setYearOfManufacture( int y ) {
-      yearOfManufacture = y;
-   }
-
-   void setColor( std::string c ) {
-      color = c;
-   }
-
-   void setEngineCapacity( int ec ) {
-      engineCapacity = ec;
-   }
-
-   std::string getMake() const {
-      return make;
-   }
-
-   std::string getFuelType() const {
-      return fuelType;
-   }
-
-   int getYearOfManufacture() const {
-      return yearOfManufacture;
-   }
-
-   std::string getColor() const {
-      return color;
-   }
-
-   int getEngineCapacity() const {
-      return engineCapacity;
-   }
-
-   void displayCarDetails() const {
-      std::cout << "CAR DETAILS"
-         << "\nMake:                " << make
-         << "\nFuel type:           " << fuelType
-         << "\nYear of manufacture: " << yearOfManufacture
-         << "\nColor:               " << color
-         << "\nEngine capacity:     " << engineCapacity
-         << std::endl;
-   }
-
-private:
-
-   std::string make;
-   std::string fuelType;
-   int yearOfManufacture;
-   std::string color;
-   int engineCapacity;
+#include<iostream>
+#include<string>
+using namespace std;
+class gradebook{
+	private:
+	
+		string name;
+		string course;
+	
+		public:
+			gradebook(string a,string b){
+				name=b;
+				course=a;
+			}
+			setName(string a){
+					name=a;
+			}
+	
+			getName(){
+				cout<<name;
+			}
+			displaymsg(){
+				cout<<"\t\tWELCOME TO OUR PROGRAM\n\n"<<course<<" This course is presented by: "<<name;
+			}
 };
+int main(){
+	gradebook book("UMAIR","OOP");
+	book.displaymsg();
+	string m;
+	cout<<"\nENTER CONSTRUCTORS NAME: ";
+	cin.sync();
+	getline(cin,m);
+book.setName(m);
+book.getName();
+}
